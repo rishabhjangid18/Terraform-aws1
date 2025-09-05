ab8c8efe66304b13ee852dbc6b71ea23f31ea4f4
Rishabh Jangid <110629525+rishabhjangid18@users.noreply.github.com>
2025-09-05 16:30:57 +0530

    Update main.tf

--- envs/dev/main.tf
+++ envs/dev/main.tf
@@ -14,24 +14,24 @@ provider "aws" {
 }
 
 module "vpc" {
-  source        = "../modules/vpc"
+  source         = "../../modules/vpc"
   vpc_cidr_block = var.vpc_cidr_block
   vpc_name       = "${var.env}-vpc"
   tags           = var.tags
 }
 
 module "subnet" {
-  source              = "../modules/subnet"
-  vpc_id              = module.vpc.vpc_id
-  public_subnet_cidrs = var.public_subnet_cidrs
+  source               = "../../modules/subnet"
+  vpc_id               = module.vpc.vpc_id
+  public_subnet_cidrs  = var.public_subnet_cidrs
   private_subnet_cidrs = var.private_subnet_cidrs
-  availability_zones  = var.availability_zones
-  env                 = var.env
-  tags                = var.tags
+  availability_zones   = var.availability_zones
+  env                  = var.env
+  tags                 = var.tags
 }
 
 module "sg" {
-  source        = "../modules/security-group"
+  source        = "../../modules/security-group"
   vpc_id        = module.vpc.vpc_id
   sg_name       = "${var.env}-sg"
   ingress_cidrs = var.sg_ingress_cidrs
