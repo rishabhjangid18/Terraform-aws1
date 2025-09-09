a6386c7d1c70cc27af457fa2c0ad4f5243159313
Rishabh Jangid <110629525+rishabhjangid18@users.noreply.github.com>
2025-09-09 13:14:00 +0530

    Update main.tf

--- envs/dev/main.tf
+++ envs/dev/main.tf
@@ -1,37 +1,12 @@
-terraform {
-  required_version = ">= 1.5.0"
-
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
-
-  backend "s3" {
-    bucket         = "terraform-cicd-8099"
-    key            = "dev/networking/terraform.tfstate"
-    region         = "ap-south-1"
-    dynamodb_table = "terraform-locks"
-    encrypt        = true
-  }
-}
-
-provider "aws" {
-  region = var.region
-}
-
-# VPC Module
 module "vpc" {
-  source         = "../../modules/vpc"
+  source         = "../modules/vpc"
   vpc_cidr_block = var.vpc_cidr_block
   vpc_name       = "${var.env}-vpc"
   tags           = var.tags
 }
 
-# Subnet Module
 module "subnet" {
-  source               = "../../modules/subnet"
+  source               = "../modules/subnet"
   vpc_id               = module.vpc.vpc_id
   public_subnet_cidrs  = var.public_subnet_cidrs
   private_subnet_cidrs = var.private_subnet_cidrs
@@ -40,9 +15,8 @@ module "subnet" {
   tags                 = var.tags
 }
 
-# Security Group Module
 module "sg" {
-  source        = "../../modules/security-group"
+  source        = "../modules/security-group"
   vpc_id        = module.vpc.vpc_id
   sg_name       = "${var.env}-sg"
   ingress_cidrs = var.sg_ingress_cidrs
