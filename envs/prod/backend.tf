bb65cc29ec2993282cb8449f75d2495e7769006f
Rishabh Jangid <110629525+rishabhjangid18@users.noreply.github.com>
2025-09-09 12:58:33 +0530

    Update backend.tf

--- envs/prod/backend.tf
+++ envs/prod/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "terraform-cicd-8099"
-    key            = "dev/networking/terraform.tfstate"
+    key            = "prod/networking/terraform.tfstate"
     region         = "ap-south-1"
     dynamodb_table = "terraform-locks"
     encrypt        = true
