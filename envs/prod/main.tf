6fcf7853bac15709ae1ef5159786a837664b1ffd
Rishabh Jangid <110629525+rishabhjangid18@users.noreply.github.com>
2025-09-09 13:14:11 +0530

    Update main.tf

--- envs/prod/main.tf
+++ envs/prod/main.tf
@@ -1,33 +1,18 @@
-terraform {
-  required_version = ">= 1.5.0"
-
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
-}
-
-provider "aws" {
-  region = var.region
-}
-
 module "vpc" {
-  source        = "../modules/vpc"
+  source         = "../modules/vpc"
   vpc_cidr_block = var.vpc_cidr_block
   vpc_name       = "${var.env}-vpc"
   tags           = var.tags
 }
 
 module "subnet" {
-  source              = "../modules/subnet"
-  vpc_id              = module.vpc.vpc_id
-  public_subnet_cidrs = var.public_subnet_cidrs
+  source               = "../modules/subnet"
+  vpc_id               = module.vpc.vpc_id
+  public_subnet_cidrs  = var.public_subnet_cidrs
   private_subnet_cidrs = var.private_subnet_cidrs
-  availability_zones  = var.availability_zones
-  env                 = var.env
-  tags                = var.tags
+  availability_zones   = var.availability_zones
+  env                  = var.env
+  tags                 = var.tags
 }
 
 module "sg" {
