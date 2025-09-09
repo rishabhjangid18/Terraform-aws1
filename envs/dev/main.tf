3af07ccde470b0aa7eb9092647502bca53406ace
Rishabh Jangid <110629525+rishabhjangid18@users.noreply.github.com>
2025-09-09 13:37:13 +0530

    Update main.tf

--- envs/dev/main.tf
+++ envs/dev/main.tf
@@ -7,14 +7,6 @@ terraform {
       version = "~> 5.0"
     }
   }
-
-  backend "s3" {
-    bucket         = "terraform-cicd-8099"
-    key            = "dev/networking/terraform.tfstate"
-    region         = "ap-south-1"
-    dynamodb_table = "terraform-locks"
-    encrypt        = true
-  }
 }
 
 provider "aws" {
