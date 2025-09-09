0e7f60b603ce5d58e3833f9c6b0eda27d3919707
Rishabh Jangid <110629525+rishabhjangid18@users.noreply.github.com>
2025-09-09 13:59:29 +0530

    Update variables.tf

--- envs/prod/variables.tf
+++ envs/prod/variables.tf
@@ -3,14 +3,14 @@ variable "region" {
   default = "ap-south-1"
 }
 
-variable "vpc_cidr" {
-  type    = string
-  default = "10.0.0.0/16"
-}
-
 variable "prefix" {
   type    = string
   default = "prod"
+}
+
+variable "vpc_cidr" {
+  type    = string
+  default = "10.0.0.0/16"
 }
 
 variable "public_subnet_cidr" {
