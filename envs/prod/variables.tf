4c8367df7a53f44d506f4c32c5be4c360ac5e344
Rishabh Jangid <110629525+rishabhjangid18@users.noreply.github.com>
2025-09-09 13:40:30 +0530

    Create variables.tf

---
+++ envs/prod/variables.tf
@@ -0,0 +1,29 @@
+variable "region" {
+  type    = string
+  default = "ap-south-1"
+}
+
+variable "vpc_cidr" {
+  type    = string
+  default = "10.0.0.0/16"
+}
+
+variable "prefix" {
+  type    = string
+  default = "prod"
+}
+
+variable "public_subnet_cidr" {
+  type    = string
+  default = "10.0.1.0/24"
+}
+
+variable "private_subnet_cidr" {
+  type    = string
+  default = "10.0.2.0/24"
+}
+
+variable "sg_allowed_cidrs" {
+  type    = list(string)
+  default = ["10.0.0.0/8"]
+}
