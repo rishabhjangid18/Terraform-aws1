881a1d5cf6cf03eebc669357ab6b09639370f5a2
Rishabh Jangid <110629525+rishabhjangid18@users.noreply.github.com>
2025-09-09 14:13:50 +0530

    Update variables.tf

--- envs/prod/variables.tf
+++ envs/prod/variables.tf
@@ -1,18 +1,22 @@
+# AWS region
 variable "region" {
   type    = string
   default = "ap-south-1"
 }
 
+# Prefix for resource names
 variable "prefix" {
   type    = string
   default = "prod"
 }
 
+# VPC CIDR block
 variable "vpc_cidr" {
   type    = string
   default = "10.0.0.0/16"
 }
 
+# Subnet CIDRs
 variable "public_subnet_cidr" {
   type    = string
   default = "10.0.1.0/24"
@@ -23,7 +27,14 @@ variable "private_subnet_cidr" {
   default = "10.0.2.0/24"
 }
 
+# Security Group allowed CIDRs
 variable "sg_allowed_cidrs" {
   type    = list(string)
   default = ["10.0.0.0/8"]
+}
+
+# Security Group service names
+variable "sg_service_names" {
+  type    = list(string)
+  default = ["app","db"]
 }
